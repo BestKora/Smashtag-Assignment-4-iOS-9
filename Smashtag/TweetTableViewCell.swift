f1777cc987050e40fd358077f18ced089595934b
BestKora <developer@bestkora.com>
2015-08-25 14:26:01 +0300

    Задание 4 Xcode 7 beta 6

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,108 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell
+{
+    // MARK: - public API
+
+    var tweet: Tweet? {
+        didSet {
+             tweetMentionsCount = (tweet?.hashtags.count ?? 0) +
+                                  (tweet?.urls.count ?? 0) +
+                                  (tweet?.userMentions.count ?? 0) +
+                                  (tweet?.media.count ?? 0)
+            updateUI()
+        }
+  }
+    var tweetMentionsCount = 0  
+    struct Palette {
+        static let hashtagColor = UIColor.purpleColor()
+        static let urlColor = UIColor.blueColor()
+        static let userColor = UIColor.orangeColor()
+    }
+
+  
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    
+
+    func updateUI() {
+        // переустанавливаем заново любую существующую инфоримацию твита
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel?.text = nil
+        
+        // загружаем новую информацию из нашего tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.attributedText  = setTextLabel(tweet)
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            setProfileImageView(tweet) // tweetProfileImageView updated asynchronously
+            tweetCreatedLabel?.text = setCreatedLabel(tweet)
+        }
+    }
+    
+ 
+    private func setTextLabel(tweet: Tweet) -> NSMutableAttributedString {
+        var tweetText:String = tweet.text
+        for _ in tweet.media {tweetText += " 📷"}
+        let attribText = NSMutableAttributedString(string: tweetText)
+        
+        attribText.setKeywordsColor(tweet.hashtags, color: Palette.hashtagColor)
+        attribText.setKeywordsColor(tweet.urls, color: Palette.urlColor)
+        attribText.setKeywordsColor(tweet.userMentions, color: Palette.userColor)
+ 
+        return attribText
+    }
+    
+    private func setCreatedLabel(tweet: Tweet) -> String {
+        let formatter = NSDateFormatter()
+        if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+            formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        } else {
+            formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        }
+        return formatter.stringFromDate(tweet.created)
+    }
+    
+    private func setProfileImageView(tweet: Tweet) {
+        if let profileImageURL = tweet.user.profileImageURL {
+            let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
+                let imageData = NSData(contentsOfURL: profileImageURL)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if profileImageURL == tweet.user.profileImageURL {
+                        if imageData != nil {
+                            self.tweetProfileImageView?.image = UIImage(data: imageData!)
+                        } else {
+                            self.tweetProfileImageView?.image = nil
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+}
+
+// MARK: - Расширение
+
+private extension NSMutableAttributedString {
+    func setKeywordsColor(keywords: [Tweet.IndexedKeyword], color: UIColor) {
+        for keyword in keywords {
+            addAttribute(NSForegroundColorAttributeName, value: color, range: keyword.nsrange)
+        }
+    }
+}
+
---
+++ Smashtag/TweetTableViewController.swift
@@ -0,0 +1,160 @@
+//
+//  TweetTableViewController.swift
+//  Smashtag
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController, UITextFieldDelegate
+{
+    // MARK: - Public API
+
+    var tweets = [[Tweet]]()
+
+    var searchText: String? = "#stanford" {
+        didSet {
+            lastSuccessfulRequest = nil
+            searchTextField?.text = searchText
+            tweets.removeAll()
+            tableView.reloadData() // очистка таблицы table view
+            refresh()
+        }
+    }
+    
+    // MARK: - View Controller Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        if tweets.count == 0 {
+            refresh()
+        } else {
+            // делаем установки для случая единственного tweet
+            searchTextField.text = " "
+            let tweet = tweets.first!.first!
+            title = "Tweet by " + tweet.user.name
+            tableView.reloadSections(NSIndexSet(indexesInRange:
+                NSMakeRange(0, tableView.numberOfSections)),
+                withRowAnimation: .None)
+        }
+    }
+    
+    // MARK: - Refreshing
+
+    private var lastSuccessfulRequest: TwitterRequest?
+
+    private var nextRequestToAttempt: TwitterRequest? {
+        if lastSuccessfulRequest == nil {
+            if searchText != nil {
+                return TwitterRequest(search: searchText!, count: 100)
+            } else {
+                return nil
+            }
+        } else {
+            return lastSuccessfulRequest!.requestForNewer
+        }
+    }
+    
+    @IBAction private func refresh(sender: UIRefreshControl?) {
+        if searchText != nil {
+            RecentSearches().add(searchText!)
+            if let request = nextRequestToAttempt {
+                self.lastSuccessfulRequest = request // oops, забыли эту строку в лекции
+                request.fetchTweets { (newTweets) -> Void in
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        if newTweets.count > 0 {
+                            self.tweets.insert(newTweets, atIndex: 0)
+                            self.tableView.reloadData()
+                            self.tableView.reloadSections(NSIndexSet(indexesInRange:
+                                    NSMakeRange(0, self.tableView.numberOfSections)),
+                                                                  withRowAnimation: .None)
+                            sender?.endRefreshing()
+                            self.title = self.searchText
+                        }
+                        sender?.endRefreshing()
+                    })
+                }
+            }
+        } else {
+            sender?.endRefreshing()
+        }
+    }
+    
+    func refresh() {
+        refreshControl?.beginRefreshing()
+        refresh(refreshControl)
+    }
+    
+    // MARK: - Storyboard Connectivity
+    
+    @IBOutlet private weak var searchTextField: UITextField! {
+        didSet {
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            textField.resignFirstResponder()
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "Tweet"
+        static let MentionsIdentifier = "Show Mentions"
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tweets.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets[section].count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as! TweetTableViewCell
+        
+        cell.tweet = tweets[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+    
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if identifier == Storyboard.MentionsIdentifier {
+            if let tweetCell = sender as? TweetTableViewCell {
+                if tweetCell.tweet!.hashtags.count + tweetCell.tweet!.urls.count + tweetCell.tweet!.userMentions.count + tweetCell.tweet!.media.count == 0 {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == Storyboard.MentionsIdentifier {
+                if let mtvc = segue.destinationViewController as? MentionsTableViewController {
+                    if let tweetCell = sender as? TweetTableViewCell {
+                        mtvc.tweet = tweetCell.tweet
+                    }
+                }
+            }
+        }
+    }
+
+}
